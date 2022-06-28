3447ae4fdce7f70f84e7ab467b948f8f7c3c30a9
Christolake <the.christolake@gmail.com>
2022-06-28 16:06:30 -0400

    init

---
+++ src/components/DecklistGenerator.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// Databases
+
+const yugipedia = 'https://yugipedia.com/api.php';
+const parseDB = '?action=parse&format=json&page=';
+const decklists = '%27s_Decks&prop=wikitext&formatversion=2';
+
